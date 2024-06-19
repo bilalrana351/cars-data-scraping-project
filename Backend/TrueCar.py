b028228b71a09d9b34d558161959159a9d0d6177
bilalrana351 <bilalrana351@gmail.com>
2024-06-20 00:01:34 +0500

    Completed script for site 1, soon to complete for site 2

--- Backend/TrueCar.py
+++ Backend/TrueCar.py
@@ -5,85 +5,64 @@ from Headers import getHeader
 from bs4 import BeautifulSoup
 import Helpers
 import validators
+from requests_html import HTMLSession
 
-# At the start we will start from the page 1
-pageNumber = 1
-
-# These will be the current number of records scraped
-currentRecords = 0
-
-# The records per page are 30 here
 recordsInAPage = 30
 
-finished = False
-
-info = []
-
 # This site gives a maximum of 30 records per page
-def scrapCars(yearMin=None,yearMax=None,make=None,model=None,trim=None,zip=None,radius=None,recordsNumber=None,newRequest=False):
-    global pageNumber
-    global currentRecords
+def scrapCars(pageNumber,yearMin=None,yearMax=None,make=None,model=None,trim=None,zip=None,radius=None,recordsNumber=None,newRequest=False,):
     global recordsInAPage
-    global finished
 
     # If it is a new request we will reset the page number
     if newRequest:
-        pageNumber = 1
-        currentRecords = 0
-        finished = False
+        pass
     
     # Get the spaces in the makes, models, and trims convert to - for the url
 
     # This all is input preprocessing
     radius,zip,yearMin,yearMax = interPretFigures(radius,zip,yearMin,yearMax)
 
-    model,make,trim = Helpers.replaceSpaces(model,make,trim)
-
-    # If they are already finished this will just return an empty array
-    if finished:
-        try:
-            info = info[recordsNumber:]
-            return info[0:recordsNumber]
-        except:
-            try:
-                infoFinal = info.copy()
-                info = [] # Empty the information
-                return infoFinal
-            except:
-                return []
+    model,make,trim = replaceSpaces(model,make,trim)
     
-    # This will detect whether the current number of records have been replenished, i.e is there new records needed
-    needNewRecords = Helpers.areNewRecordsNeeded(currentRecords,recordsInAPage)
 
-    if(needNewRecords):
-        initialAddress = getInitialAddress(yearMin,yearMax,make,model,trim,zip,radius)
+    initialAddress = getInitialAddress(pageNumber,yearMin,yearMax,make,model,trim,zip,radius)
 
-        print(initialAddress)
+    print(initialAddress)
 
-        response = requests.get(initialAddress)
+    # session = HTMLSession()
 
-        soup = BeautifulSoup(response.text,'html.parser')
+    # response = session.get(initialAddress)
 
-        with open("output.txt","w") as f:
-            f.write(soup.prettify())
+    # response.html.render()
 
-        finished = checkAdditionalListing(soup)
+    # soup = BeautifulSoup(response.html.html,'html.parser')
 
-        if (finished):
-            info = scrapInfo(soup,True)
-            return info[0:recordsNumber]
+    # if newRequest:
 
-        info = scrapInfo(soup,False)
-        # At the end we will increment the current record
-        currentRecords += recordsNumber
 
-        pageNumber += 1
+    # info = scrapInfo(soup,False)
+    # # At the end we will increment the current record
+    # currentRecords += recordsNumber
 
-        return info[0:recordsNumber]
+    # pageNumber += 1
+
+    # return info
+
+def replaceSpaces(model,make,trim):
+    # In all the makes models and trims we will replace space with -
+    if model == None: # In case of None we will let None be None
+        pass
+    else: 
+        model = model.replace(" ","-").lower()
+    if make == None:
+        pass
     else:
-        info = info[recordsNumber:]
-        currentRecords += recordsNumber
-        return info[0:recordsNumber]
+        make = make.replace(" ","-").lower()
+    if trim == None:
+        pass
+    else:
+        trim = trim.replace(" ","-").lower()
+    return [model,make,trim]
     
 def scrapInfo(html,lastPage):
     allListings = html.find("ul","row mb-3 mt-1").children
@@ -116,7 +95,7 @@ def scrapImageUrl(card):
         if (validators.url(imageBlock) == True):
             return imageBlock
         else:
-            imageBlock = card.find("img",class_="img-inner img-block")['data-original']
+            imageBlock = card.find("img",class_="img-inner img-block")['data-src']
             if(validators.url(imageBlock)==True):
                 return imageBlock
             else:
@@ -126,11 +105,13 @@ def scrapImageUrl(card):
                 else:
                     return "Image not found"
     except Exception as e:
-        print(e)
         return "Image not found"
+    
+def scrapDescription(card):
+    # FInd the div with w-full truncate name
+    pass
 
-def getInitialAddress(yearMin,yearMax,make,model,trim,zip,radius):
-    global pageNumber
+def getInitialAddress(pageNumber,yearMin,yearMax,make,model,trim,zip,radius):
     # This is the base address
     address = "https://www.truecar.com/used-cars-for-sale/listings/"
     
@@ -141,10 +122,6 @@ def getInitialAddress(yearMin,yearMax,make,model,trim,zip,radius):
     # This is the address for the model
     if model != None:
         address += model + "/"
-    
-    # This is the address for the trim
-    if trim != None:
-        address += trim + "/"
     
     # This is the address for the year
     address += "year-" + str(yearMin) + "-" + str(yearMax) + "/"
@@ -192,22 +169,5 @@ def interPretFigures(radius,zip,yearMin,yearMax):
     zip = city + "-" + state
 
     return [radius,zip,yearMin,yearMax]
-
-def checkAdditionalListing(html):
-    allListings = html.find("ul","row mb-3 mt-1").children
-
-    totalListings = 0
-
-    for listing in allListings:
-        totalListings += 1
     
-    if (totalListings - 1 == 30):
-        print("Were 30")
-        return False
-    else:
-        print("Were not 30")
-        print(totalListings)
-        return True
-        
-    
-scrapCars(2024,2024,"Honda","Civic","ex-l",60601,-1,30,True)
+scrapCars(1,make="mercedes-benz",zip=60601,radius=75,recordsNumber=30,newRequest=True)
