4b3637e3f2b69a2d10da65f3f05b043e5bb512d2
bilalrana351 <bilalrana351@gmail.com>
2024-06-28 13:23:52 +0500

    Fixed carsksl

--- Backend/Carksl.py
+++ Backend/Carksl.py
@@ -1,82 +1,32 @@
 from bs4 import BeautifulSoup
 import json
-from playwright.sync_api import sync_playwright
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-}
+import requests
 
 maxPages = 3
 
 def scrapCars(pageNumber, yearMin=None, yearMax=None, make=None, model=None, trim=None, zip=None, radius=None, newRequest=False):
-    global headers
     global maxPages
 
     if pageNumber > maxPages:
         return []
     
     base_url = makeBaseUrl(make,model,trim,yearMin,yearMax,zip,radius,pageNumber)
+    
+    data = getJsonData(base_url, pageNumber, make, model, yearMin, yearMax, trim, zip, radius)
 
-    print(base_url)
-
-    # Use Playwright to scrape the page
-    response = None
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)  # Use headless=True to run without GUI
-        page = browser.new_page()
-        page.set_extra_http_headers(headers)
-        page.goto(base_url, timeout=30000000, wait_until="load")
-        response = page.content()
-        browser.close()
-
-    soup = BeautifulSoup(response, 'html.parser')
-
-    with open("test.html", "w") as f:
-        f.write(str(soup))
+    info = scrapInfo(data)
 
     # Find the total number of pages
     if newRequest:
-        try:
-            # Extract the correct meta tag content attribute containing the total number of listings
-            meta_description = soup.find_all('meta', attrs={'name': 'description'})
-            if meta_description:
-                for meta in meta_description:
-                    if 'new and used' in meta['content'] and 'for sale' in meta['content']:
-                        listings_info = meta['content']
-                        total_listings = int(listings_info.split(' ')[0].replace(',', ''))
-                        break
-            else:
-                raise ValueError("No valid meta description found.")
-            
-            listings_per_page = 24
-            maxPages = (total_listings + listings_per_page - 1) // listings_per_page
-        except Exception as e:
-            print(f"Error finding total pages: {e}")
+        maxPages = findMaxPages(data)
 
-    # Find all car listings
-    car_listings = soup.find_all('div', class_='Listing__ListingWrapper-sc-a69vd9-0')
+    return info
 
-    cars = []
-
-    # Process each car listing
-    for listing in car_listings:
-        # Extract image URL
-        image_wrapper = listing.find('div', class_='Listing__ListingImageWrapper-sc-1v5k5vh-2')
-        image_url = 'N/A'
-        if image_wrapper:
-            image_style = image_wrapper.find('a').get('style', '')
-            image_url = image_style.split('url("')[1].split('")')[0] if 'url("' in image_style else 'N/A'
-
-        car = {
-            'imageUrl': image_url,
-            'description': listing.find('a', class_='listing-title').text.strip() if listing.find('a', class_='listing-title') else 'N/A',
-            'price': listing.find('p', class_='Typography__variantProp-sc-5cwz35-0 eaOVFJ').text.strip() if listing.find('p', class_='Typography__variantProp-sc-5cwz35-0 eaOVFJ') else 'N/A',
-            'mileage': listing.find('p', class_='Typography__variantProp-sc-5cwz35-0 kMOSqH').text.strip() if listing.find('p', class_='Typography__variantProp-sc-5cwz35-0 kMOSqH') else 'N/A',
-            'mainLink': listing.find('a', class_='listing-title').get('href', 'N/A') if listing.find('a', class_='listing-title').get('href', 'N/A') != 'N/A' else 'N/A'
-        }
-        cars.append(car)
-
-    return cars
+def findMaxPages(data):
+    try:
+        return int(data["data"]["count"]) // 24 + 1
+    except:
+        return 0
 
 def makeBaseUrl(make, model, trim, yearMin, yearMax, zip, radius, pageNumber):
     if make is not None:
@@ -90,7 +40,7 @@ def makeBaseUrl(make, model, trim, yearMin, yearMax, zip, radius, pageNumber):
         zip = None
 
     if radius is None or radius < 0:
-        radius = None
+        radius = 2000
 
     base_url = "https://cars.ksl.com/search"
 
@@ -115,10 +65,156 @@ def makeBaseUrl(make, model, trim, yearMin, yearMax, zip, radius, pageNumber):
     
     return base_url
 
+def getJsonData(initialUrl, pageNumber, make, model, yearFrom, yearTo, trim=None, zip=None, radius=None):
+    headers = {
+        'accept': '*/*',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'no-cache',
+        'content-type': 'application/json',
+        'origin': 'https://cars.ksl.com',
+        'pragma': 'no-cache',
+        'priority': 'u=1, i',
+        'referer': initialUrl,#'https://cars.ksl.com/search/make/Honda/model/Accord/yearFrom/2012/yearTo/2023/zip/60601/miles/2000'
+        'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36',
+        'x-ddm-event-accept-language': 'en-US',
+        'x-ddm-event-ip-address': 'undefined',
+        'x-ddm-event-user-agent': '[object Object]',
+    }
+
+    params = ''
+
+    body = constructBody(make, model, yearFrom, yearTo, trim, zip, radius, pageNumber)
+
+
+
+    json_data = {
+        'endpoint': '/classifieds/cars/search/searchByUrlParams',
+        'options': {
+            'method': 'POST',
+            'headers': {
+                'Content-Type': 'application/json',
+                'User-Agent': 'cars-node',
+                'X-App-Source': 'frontline',
+                'X-DDM-EVENT-USER-AGENT': {
+                    'ua': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36',
+                    'browser': {
+                        'name': 'Chrome',
+                        'version': '126.0.0.0',
+                        'major': '126',
+                    },
+                    'engine': {
+                        'name': 'Blink',
+                        'version': '126.0.0.0',
+                    },
+                    'os': {
+                        'name': 'Android',
+                        'version': '6.0',
+                    },
+                    'device': {
+                        'vendor': 'LG',
+                        'model': 'Nexus 5',
+                        'type': 'mobile',
+                    },
+                    'cpu': {},
+                },
+                'X-DDM-EVENT-ACCEPT-LANGUAGE': 'en-US',
+                'X-MEMBER-ID': None,
+                'cookie': '',
+            },
+            'body': body
+        },
+    }
+
+    response = requests.post('https://cars.ksl.com/nextjs-api/proxy', params=params, headers=headers, json=json_data)
+
+    try:
+        return response.json()
+    except Exception as e:
+        return []
+    
+def constructBody(make, model, yearFrom, yearTo, trim, zip, radius, pageNumber):
+    body = []
+    body.append('perPage')
+    body.append('24')
+    if pageNumber is not None:
+        body.append('page')
+        body.append(str(pageNumber))
+    if make is not None:
+        body.append('make')
+        body.append(make)
+    if model is not None:
+        body.append('model')
+        body.append(model)
+    if trim is not None:
+        body.append('trim')
+        body.append(trim)
+    if yearFrom is not None:
+        body.append('yearFrom')
+        body.append(yearFrom)
+    if yearTo is not None:
+        body.append('yearTo')
+        body.append(yearTo)
+    if zip is not None:
+        body.append('zip')
+        body.append(zip)
+    if radius is not None:
+        body.append('miles')
+        body.append(radius)
+    body.append('pageNumber')
+    body.append(pageNumber)
+
+    return body
+
+def scrapInfo(data):
+    info = []
+    items = data["data"]["items"]
+    for item in items:
+        try:
+            item['trim']
+        except:
+            item['trim'] = ""
+        try:
+            description = str(item['makeYear']) + ' ' + item['make'] + ' ' + item['model'] + ' ' + item['trim']
+        except:
+            description = "description not found"
+        try:
+            imageUrl = item['photo'][0]["id"]
+        except:
+            try:
+                imageUrl = json.loads(item['photo'][0])['id']
+            except:
+                imageUrl = "image not found"
+        try:
+            price = item['price']
+        except:
+            price = "price not found"
+        try:
+            mileage = item['mileage']
+        except:
+            mileage = "mileage not found"
+        try:
+            listing = "https://cars.ksl.com/listing/" + str(item['id'])
+        except:
+            listing = "listing not found"
+        info.append({
+            'description': description,
+            'imageUrl': imageUrl,
+            'price': price,
+            'mileage': mileage,
+            'mainUrl': listing
+        })
+
+    return info
+
 def main():
     # Example usage:
-    print(scrapCars(pageNumber=1,make="Mercedes-Benz",newRequest=True))
-    print(scrapCars(2,make="Toyota",newRequest=False))
+    print(scrapCars(pageNumber=2,make="Mercedes-Benz",newRequest=True))
 
 if __name__ == "__main__":
     main()
--- Backend/TestModel.py
+++
@@ -1,8 +0,0 @@
-from Carmax import scrapCars
-
-
-result,count = scrapCars(pageNumber=1,newRequest=True,make="Honda")
-
-print("count",count)
-
-print("data",result)
